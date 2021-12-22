d8feec44f63311760eafd6033a041c7ff5b7b906
theoboldalex <theoboldalex@gmail.com>
2021-12-22 13:12:08 +0000

    copy files from local to container on publisher

--- publisher/src/Command/MessagePublisherCommand.php
+++ publisher/src/Command/MessagePublisherCommand.php
@@ -2,6 +2,8 @@
 
 namespace App\Command;
 
+use SykesCottages\Qu\Connector\Queue;
+use SykesCottages\Qu\Connector\RabbitMQ;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -14,6 +16,14 @@ class MessagePublisherCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $rabbitMq = new RabbitMQ('rabbit-1', 5672, 'guest', 'guest');
+
+        $rabbitMq->setQueueOptions(['blockingConsumer' => true]);
+
+        $testingQueue = new Queue('test', $rabbitMq);
+
+        $testingQueue->queueMessage(['example' => rand(1, 10000)]);
+
         return Command::SUCCESS;
     }
 }
