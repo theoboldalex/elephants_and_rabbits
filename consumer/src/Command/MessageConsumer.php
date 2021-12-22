8cbdb049ff09a5ba5f2957fa8568b5950e913b06
theoboldalex <theoboldalex@gmail.com>
2021-12-22 22:51:53 +0000

    consumer consumes

---
+++ consumer/src/Command/MessageConsumer.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Command;
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class MessageConsumer extends Command
+{
+    private const QUEUE_NAME = 'test';
+
+    protected static $defaultName = 'queue:consume';
+
+    protected static $defaultDescription = 'consume the default queue';
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $conn = new AMQPStreamConnection(
+            'localhost',
+            '5672',
+            'guest',
+            'guest',
+        );
+
+        $channel = $conn->channel();
+        $channel->queue_declare(self::QUEUE_NAME, false, false, false, false);
+
+        echo " [*] Waiting for messages. To exit press CTRL+C\n";
+
+        $callback = function ($msg) {
+            echo ' [x] Received ', $msg->body, "\n";
+        };
+
+        $channel->basic_consume(self::QUEUE_NAME, '', false, true, false, false, $callback);
+
+        while ($channel->is_open()) {
+            $channel->wait();
+        }
+
+        return Command::SUCCESS;
+    }
+}
