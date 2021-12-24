6f7d11c61d7ad8bc70ea09387b7b5f7b4cf037d0
theoboldalex <theoboldalex@gmail.com>
2021-12-24 09:13:50 +0000

    clean up consumer

--- consumer/src/Command/MessageConsumer.php
+++ consumer/src/Command/MessageConsumer.php
@@ -2,6 +2,7 @@
 
 namespace App\Command;
 
+use PhpAmqpLib\Channel\AMQPChannel;
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -11,11 +12,31 @@ class MessageConsumer extends Command
 {
     private const QUEUE_NAME = 'test';
 
+    private AMQPChannel $channel;
+
     protected static $defaultName = 'queue:consume';
 
     protected static $defaultDescription = 'consume the default queue';
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->channel = $this->openChannel();
+
+        echo " [*] Waiting for messages. To exit press CTRL+C\n";
+
+        $this->consume();
+
+        while ($this->channel->is_open()) {
+            $this->channel->wait();
+        }
+
+        return Command::SUCCESS;
+    }
+
+    /**
+     * @return AMQPChannel
+     */
+    private function openChannel(): AMQPChannel
     {
         $conn = new AMQPStreamConnection(
             getenv('RABBIT_HOST'),
@@ -26,19 +47,26 @@ class MessageConsumer extends Command
 
         $channel = $conn->channel();
         $channel->queue_declare(self::QUEUE_NAME, false, false, false, false);
+        return $channel;
+    }
 
-        echo " [*] Waiting for messages. To exit press CTRL+C\n";
-
+    /**
+     * @return void
+     */
+    private function consume(): void
+    {
         $callback = function ($msg) {
             echo ' [x] Received ', $msg->body, "\n";
         };
 
-        $channel->basic_consume(self::QUEUE_NAME, '', false, true, false, false, $callback);
-
-        while ($channel->is_open()) {
-            $channel->wait();
-        }
-
-        return Command::SUCCESS;
+        $this->channel->basic_consume(
+            self::QUEUE_NAME,
+            '',
+            false,
+            true,
+            false,
+            false,
+            $callback
+        );
     }
 }
